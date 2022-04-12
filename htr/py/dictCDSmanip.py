41d40458081c6a3cf554cabfa6fb58ca5ce5beea
Sebastien Biay <sbiayha@outlook.fr>
2022-04-12 10:32:19 +0200

    dictCDSmanip : bonne gestion des arguments

--- htr/py/dictCDSmanip.py
+++ htr/py/dictCDSmanip.py
@@ -1,54 +1,83 @@
 import click
+import json
 import importlib
 import os
 from constantes import DICTPAGES
 
+# On charge le dictionnaire Json global de la correspondance CDS
+with open(f"./py/dictComplets/dictCDS.json") as f:
+    dictCDS = json.load(f)
+
+def controleFormes(dictPage):
+    """
+    Cette fonction prend comme argument un dictionnaire de page, compare son contenu au dictCDS
+    et retourne un dictCDS enrichi
+    :param dictPage: dictionnaire de page
+    :type dictPage: dict
+    :return: dictCDS enrichi par de nouvelles formes
+    :return type: dict
+    """
+    # On boucle sur chaque clé du dictionnaire de page
+    for forme in dictPage:
+        # On ne traite les formes que si elles sont associés à un lemme qui les corrige
+        if dictPage[forme]["lem"]:
+            # Si la forme existe déjà dans dictCDS mais que les lemmes ne sont pas identiques
+            if forme in dictCDS and dictPage[forme]["lem"] != dictCDS[forme]["lem"]:
+                # Alors on arrête le code et on délivre un message d'alerte
+                print(f"La forme {forme}, que l'on souhaite corriger en {dictPage[forme]['lem']}, "
+                      f"possède déjà une correction de référence : "
+                      f"{dictCDS[forme]['lem']}")
+                print("Veuillez corriger les dictionnaires et relancer le script (si les deux solutions sont "
+                      "possibles, inscrivez null en tant que lemme dans les deux dictionnaires, la forme ne sera "
+                      "plus corrigée automatiquement).")
+                break
+            # Si la forme est absente de dictCDS, on l'y inscrit
+            elif forme not in dictCDS:
+                dictCDS[forme] = {
+                    "lem": dictPage[forme]["lem"],
+                    "ctxt": dictPage[forme]["ctxt"],
+                }
+    return dictCDS
+
 @click.command()
-def dictCDStransform():
+@click.option("-f", "--file", type=str, help="Nome de fichier contenu dans le dossier "
+                                                                "./py/dictPages/")
+@click.option("-A", "--all", is_flag=True, default=False, help="Prend tous les fichiers du dossier ./py/dictPages/")
+def dictCDSintegration(file, all):
     """
-    USAGE UNIQUE
-    
-    Ce script transforme le fichier dictCDS original, dont la structure est simplement une série de couples clé-valeur
-    en un dictionnaire plus élaboré, dont les valeurs sont des dictionnaires avec des clé "lem" pour lemme
-    et "ctxt" pour contexte.
-    """
-    
-    from py.dictComplets.dictCDS import dict
-    
-    nouveauDict = {}
-    
-    for cle in dict:
-        nouveauDict[cle] = {
-            "lem": dict[cle],
-            "ctxt": []
-        }
-    
-    nouveauDict = str(nouveauDict).replace("},", "},\n").replace(": {", ":\n\t{").replace("', '", "',\n\t '")
-    
-    with open("./py/dictCDS.py", mode="w") as f:
-        f.write(
-            f"dictCDS = {nouveauDict}"
-        )
-
-
-def dictCDSintegration():
-    """
-    Ce script prend comme paramètres une source consistant en un dictionnaire python d'entrée
-    le compare au contenu de dictCDS, intègre les entrées nouvelles et retourne des messages d'alertes
+    Ce script prend comme paramètres une source consistant en un fichier Json d'entrée contenu dans le dossier dictPages
+    le compare au contenu de dictCDS.json, intègre les entrées nouvelles et retourne des messages d'alertes
     pour les entrées générant un conflit d'intégration.
-    :param source: dictionnaire python issu de la correction automatisée d'une page de transcription
-    :type source: chemin de fichier
+    :param fichierdictpages: fichier Json issu de la correction automatisée d'une page de transcription
+    :type fichierdictpages: Json
+    :returns: None
     """
-    # On boucle sur les fichiers du dossier dictPages
-    for root, dirs, files in os.walk(DICTPAGES):
-        for filename in files:
-            with open(DICTPAGES.strip() + "/Dict" + filename.replace(".xml", ".py"), "w") as file_out:
-                print("writing to " + DICTPAGES + "/Dict" + filename.replace(".xml", ".py"))
-                file_out.write("dictPage = ")
-                file_out.write(dictionary)
     
-    #with open("./py/dictPages/DictCdS02_Konv002-02_0067.json", mode="w", encoding="UTF-8") as f:
+    # Si l'option pour transformer tous les dictionnaires du dossier dictPages est active
+    if all:
+        # On boucle sur les fichiers du dossier dictPages
+        for root, dirs, files in os.walk(DICTPAGES):
+            for filename in files:
+                # On pose comme condition que le fichier est une extension .json
+                if filename[-4:] == "json":
+                    with open(DICTPAGES + filename) as f:
+                        dictPage = json.load(f)
+                    dictCDS = controleFormes(dictPage)
+                    print(f"Le fichier {filename} a été traité avec succès.")
+                    
+    # Si on ne transforme qu'un seul fichier passé en argument
+    else:
+        # On charge le dictionnaire Json passé en argument
+        with open(f"./py/dictPages/{file}") as f:
+            dictPage = json.load(f)
+        dictCDS = controleFormes(dictPage)
+        print(f"Le fichier {file} a été traité avec succès.")
+    
+    
+    
+    # with open("./py/dictPages/DictCdS02_Konv002-02_0067.json", mode="w", encoding="UTF-8") as f:
     #    On écrit le contenu du dictionnaire au format Jsonn en s'assurant d'encoder en UTF-8 et non en ASCII
     #    json.dump(dictPage, f, indent=2, ensure_ascii=False)
+
 
 dictCDSintegration()
