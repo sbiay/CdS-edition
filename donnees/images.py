46746c84b6b918a74f3f24c364553d6fe6d9d585
Sebastien Biay <sbiayha@outlook.fr>
2022-04-14 13:39:19 +0200

    Script Images.py opérationnel

--- donnees/images.py
+++ donnees/images.py
@@ -1,3 +1,7 @@
+import click
+import csv
+import os
+
 from toutesDonnees import donneesFud, donneesZenodo
 
 
@@ -21,6 +25,52 @@ def noticeImage(image):
             if item["Images"]:
                 # On pose enfin comme condition que l'image recherchée soit dans la liste
                 if image in item["Images"]:
-                    print(zenodo[item["idno"]])
+                    zenodo[item["idno"]]["nom_image"] = image
+                    return zenodo[item["idno"]]
+
+@click.command()
+@click.argument("DOSSIER")
+def donneesImages(dossier):
+    """
+    Cette fonction prend comme argument une liste de fichiers images
+    et retourne dans un fichier CSV la liste des identifiants et des URL du site http://constance-de-salm.de
+    :param dossier: chemin du dossier où se trouvent les images et où est exporté le CSV de sortie
+    :return: None
+    """
+    listeImages = []
+    # On analyse l'arborescence du chemin de dossier passée en argument
+    for racine, dirs, fichiers in os.walk(dossier):
+        for fichier in fichiers:
+            # On ne sélectionne dans le dossier que les fichiers portant l'extension jpg
+            if fichier[-3:] == "jpg":
+                listeImages.append(fichier)
     
-noticeImage("CdS02_Konv002-02_0067.jpg")
+    # On initie la liste des résultats
+    resultats = []
+    for image in listeImages:
+        if noticeImage(image):
+            resultats.append(noticeImage(image))
+        else:
+            resultats.append(
+                {
+                    'nom_image': image,
+                    'Nr. der Digitalisate': None,
+                    'URL': None
+                }
+            )
+    print(resultats)
+    # On écrit les résultats dans un fichier CSV
+    with open(f"{dossier}/donnees.csv", 'w', newline='') as csvfile:
+        fieldnames = ['nom_image', 'Nr. der Digitalisate', 'URL']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for resultat in resultats:
+            writer.writerow(
+                {
+                    'nom_image': resultat['nom_image'],
+                    'Nr. der Digitalisate': resultat['Nr. der Digitalisate'],
+                    'URL': resultat['URL']
+                }
+            )
+
+donneesImages()
