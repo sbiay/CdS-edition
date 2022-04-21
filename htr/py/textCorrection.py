574333be7ec3d131b8f9c010a8c37535f4b9acf7
Sebastien Biay <sbiayha@outlook.fr>
2022-04-21 12:46:29 +0200

    Correction : les standards sont OK

--- htr/py/textCorrection.py
+++ htr/py/textCorrection.py
@@ -1,8 +1,10 @@
 import os
 import json
 import click
-
-from spellcheckTexts import suppress_punctuation
+import spacy
+from spacy.lang.fr.examples import sentences
+# Installation de Spacy https://spacy.io/usage#quickstart
+# module linguistique fr_core_news_sm
 from constantes import XMLaCORRIGER, XMLCORRIGEES, DICTPAGESCORR, DICTCDS
 
 # TODO Passer des arguments pour gérer tous les fichiers ou un seul
@@ -48,61 +50,66 @@ def textCorrectionXML():
                     else:
                         # On initie la liste des entrées dont on actualisera le contexte dans le dictCDS
                         entreesMAJ = {}
-                        # Si la ligne contient Unicode, il s'agit de la transcription à corriger
                         ligne = ligneBrute
                         # On remplace l'encodage XML des apostrophes
                         ligne = ligne.replace("&#39;", "'")
-                        # On supprime les signes de ponctuation
-                        ligne = suppress_punctuation(ligne)
-                        # Puis on supprime les éventuelles doubles espaces
-                        ligne = ligne.replace("  ", " ")
+                        # On supprime l'indentation
+                        ligne = ligne.replace("          ", "")
                         # On supprime également les balises collées au premier et au dernier mot
-                        ligne = ligne.replace("<Unicode>", "")
-                        ligne = ligne.replace("</Unicode>", "")
-                        # Et on découpe chaque mot selon les espaces restantes
-                        ligne = ligne.split(" ")
-                        # On initie la ligne corrigée
-                        ligneCorr = ligneBrute
+                        ligne = ligne.replace("<Unicode>", "").replace("</Unicode>", "")
                         
-                        # On effectue une première recherche pour les formes possédant une espace
-                        # (ce sont les mots coupés en deux qu'il faut corriger en un seul mot)
-                        for forme in dictPage:
-                            # On n'intervient que si la valeur de lemme n'est pas "null"
-                            if dictPage[forme]["lem"]:
-                                # On peut se retrouver avec une liste dont le seul élément est "null"
-                                if dictPage[forme]["lem"][0]:
-                                    # La correction retenue ou "lemme"
-                                    # est le premier item de la liste-valeur de la clé "lem"
-                                    lemme = dictPage[forme]["lem"][0]
-                                    # Si la forme contient une espace, on la traite en premier
-                                    # en appliquant la correction à la ligne dans son ensemble
-                                    # (et non à un mot particulier)
-                                    if len(forme.split(" ")) > 1:
-                                        if forme in ligneCorr:
-                                            ligneCorr = ligneCorr.replace(forme, lemme)
-                                    # Si la forme ne contient pas d'espace
-                                    else:
-                                        # On boucle sur chaque mot
-                                        for mot in ligne:
-                                            # Comme la liste des mots contient des vides,
-                                            # on pose une condition d'existence
-                                            if mot:
-                                                # Si le mot courant correspond à l'entrée de dictionnaire
-                                                if forme == mot:
-                                                    if "'" in forme:
-                                                        forme = forme.replace("'", "&#39;")
-                                                    # Si le mot est en milieu de ligne
-                                                    ligneCorr = ligneCorr.replace(f" {forme}", f" {lemme}")
-                                                    # Si le mot est placé juste après la balise unicode
-                                                    ligneCorr = ligneCorr.replace(f">{forme}", f">{lemme}")
-                                                    ligneCorr = ligneCorr.replace(f"&#39;{forme}", f"&#39;{lemme}")
-                                                    if "&#39;" in forme:
-                                                        forme = forme.replace("&#39;", "'")
-                                                    entreesMAJ[forme] = {
-                                                        "lem": lemme,
-                                                        "ctxt": []
-                                                    }
-    
+                        # TODO avant de tokéniser on doit procéder au remplacement des formes avec apostrophe
+                        # TODO et des formes avec espace au milieu
+                        """
+                        # Si la forme contient une espace, on la traite en premier
+                        # en appliquant la correction à la ligne dans son ensemble
+                        # (et non à un mot particulier)
+                        if len(forme.split(" ")) > 1:
+                            if forme in ligneCorr:
+                                ligneCorr = ligneCorr.replace(forme, lemme)
+                        """
+                        # On tokénise la ligne
+                        nlp = spacy.load("fr_core_news_sm")
+                        doc = nlp(ligne)
+                        ligne = [token.text for token in doc]
+                        # On initie la ligne corrigée
+                        ligneCorr = []
+                        # On boucle sur chaque mot
+                        for mot in ligne:
+                            if "déconomie" in ligne:
+                                print(mot)
+                                # On effectue une première recherche pour les formes possédant une espace
+                                # (ce sont les mots coupés en deux qu'il faut corriger en un seul mot)
+                            # On initie le lemme ou correction
+                            lemme = None
+                            for forme in dictPage:
+                                # On n'intervient que si la valeur de lemme n'est pas "null"
+                                if forme == mot and dictPage[forme]["lem"]:
+                                    # On peut se retrouver avec une liste dont le seul élément est "null"
+                                    if dictPage[forme]["lem"][0]:
+                                        # La correction retenue ou "lemme"
+                                        # est le premier item de la liste-valeur de la clé "lem"
+                                        lemme = dictPage[forme]["lem"][0]
+                            if lemme:
+                                ligneCorr.append(lemme)
+                            # Si le lemme est resté None, il n'y a pas de correction à appliquer
+                            else:
+                                ligneCorr.append(mot)
+                            entreesMAJ[forme] = {
+                                "lem": lemme,
+                                "ctxt": []
+                            }
+                            
+                        # On recompose la ligne en tant que chaîne
+                        ligneCorr = ' '.join(ligneCorr)
+                        # On élimine les espaces en trop
+                        ligneCorr = ligneCorr.replace(' ,', ',').replace(' .', ',')\
+                            .replace('( ', '(').replace(' )', ')')
+                        if "prudente" in ligneCorr:
+                            print(f"LIGNE DEPART : {ligneBrute}")
+                            print(f"LIGNE TOKENISEE : {ligne}")
+                            print(f"LIGNE CORRIGEE : {ligneCorr}")
+                        """
                         # On renvoie le contexte du mot traité dans le dictionnaire global
                         # en reparsant la ligne corrigée
                         for entree in entreesMAJ:
@@ -127,10 +134,13 @@ def textCorrectionXML():
                                     dictCDS[entree]["lem"].append(entreesMAJ[entree]["lem"])
                                     dictCDS[entree]["ctxt"] = "AMBIGU"
                         
-                        # On réencode les apostrophes
-                        ligneCorr = ligneCorr.replace("'", "&#39;")
+                        """
+                        # On réencode les apostrophes et le balisage Unicode
+                        ligneCorr = ligneCorr.replace("' ", "&#39;")
+                        ligneCorr = f"<Unicode>{ligneCorr}</Unicode>"
                         # On écrit la ligne dans le fichier XML de sortie
                         xml_corr.write(ligneCorr + "\n")
+                        
             
             print(f"Le fichier {filename} a été corrigé avec succès")
             
