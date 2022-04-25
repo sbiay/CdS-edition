cc61bac423371adb4c495bf0d9633ef5161df9d5
Sebastien Biay <sbiayha@outlook.fr>
2022-04-25 15:09:36 +0200

    Images.py : réécriture en format Json

--- donnees/images.py
+++ donnees/images.py
@@ -1,3 +1,5 @@
+import json
+
 import click
 import csv
 import os
@@ -7,70 +9,80 @@ from toutesDonnees import donneesFud, donneesZenodo
 
 def noticeImage(image):
     """
-    Cette fonction prend comme argument un nom de fichier image et retourne un dictionnaire renseignant :
-    - Son numéro de digitalisation ;
+    Cette fonction prend comme argument un nom de fichier image et retourne une liste de dictionnaires renseignant :
+    - Leur identifiant FuD ;
+    - Leur numéro de digitalisation ;
     - L'URL de sa publication sur le site http://constance-de-salm.de/
     :param image: nom de fichier
     :type image: str
-    :return: dictionnaire de métadonnées sur l'image
-    :return type: dict
+    :return: liste de dictionnaires de métadonnées sur l'image
+    :return type: list
     """
     # On charge les données exportées de FuD et de Zenodo
     fud = donneesFud()
     zenodo = donneesZenodo()
+    notices = []
     for item in fud:
         # On pose comme condition que l'item ait une liste d'images
         if item.get("Images"):
             # et que cette liste ne soit pas vide
             if item["Images"]:
                 # On pose enfin comme condition que l'image recherchée soit dans la liste
-                if image in item["Images"]:
-                    zenodo[item["idno"]]["nom_image"] = image
-                    return zenodo[item["idno"]]
+                for cliche in item["Images"]:
+                    if image == cliche:
+                        notices.append(item)
+    
+    # On boucle sur les notices recueillies
+    for item in notices:
+        # On récupère les URL des notices publiées dans le jeu de données Zenodo
+        item["URL"] = zenodo[item["idno"]]["URL"]
+    
+    return notices
 
 @click.command()
-@click.argument("DOSSIER")
-def donneesImages(dossier):
+@click.argument("SOURCE")
+@click.argument("SORTIE")
+def donneesImages(source, sortie):
     """
-    Cette fonction prend comme argument une liste de fichiers images
-    et retourne dans un fichier CSV la liste des identifiants et des URL du site http://constance-de-salm.de
-    :param dossier: chemin du dossier où se trouvent les images et où est exporté le CSV de sortie
+    Cette fonction prend comme argument un chemin de dossier source contenant des images
+    et retourne dans un fichier Json la liste des identifiants et des URL du site http://constance-de-salm.de
+    :param source: chemin du dossier où se trouvent les images
+    :param sortie: chemin du dossier où placer le fichier de résultat
     :return: None
     """
     listeImages = []
     # On analyse l'arborescence du chemin de dossier passée en argument
-    for racine, dirs, fichiers in os.walk(dossier):
+    for racine, dirs, fichiers in os.walk(source):
         for fichier in fichiers:
             # On ne sélectionne dans le dossier que les fichiers portant l'extension jpg
             if fichier[-3:] == "jpg":
                 listeImages.append(fichier)
     
     # On initie la liste des résultats
-    resultats = []
+    avecNotice = {}
+    sansNotice = []
     for image in listeImages:
+        # On récupère les données relatives à l'image grâce à la fonction noticeImage()
         if noticeImage(image):
-            resultats.append(noticeImage(image))
+            # On boucle sur les notices renvoyées par la fonction
+            for notice in noticeImage(image):
+                avecNotice[notice["Nr. der Digitalisate"]] = {
+                        "URL": notice["URL"],
+                        "Images": notice["Images"]
+                    }
+                
         else:
-            resultats.append(
-                {
-                    'nom_image': image,
-                    'Nr. der Digitalisate': None,
-                    'URL': None
-                }
-            )
-
-    # On écrit les résultats dans un fichier CSV
-    with open(f"{dossier}/donnees.csv", 'w', newline='') as csvfile:
-        fieldnames = ['nom_image', 'Nr. der Digitalisate', 'URL']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, quotechar="|", delimiter="\t")
-        writer.writeheader()
-        for resultat in resultats:
-            writer.writerow(
-                {
-                    'nom_image': resultat['nom_image'],
-                    'Nr. der Digitalisate': resultat['Nr. der Digitalisate'],
-                    'URL': resultat['URL']
-                }
-            )
-
+            sansNotice.append(image)
+    
+    # On écrit l'objet final
+    resultats = {
+        "results": {
+            "records": avecNotice,
+            "no-record": sansNotice
+        }
+    }
+    
+    with open(f"{sortie}/donnees.json", mode="w") as jsonf:
+        json.dump(resultats, jsonf)
+    
 donneesImages()
